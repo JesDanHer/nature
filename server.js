a30ebae5f185acbe07d6f656c23870203e949026
Jesus Hernandez <jesus.hernandez@sitdigital.team>
2019-05-03 12:01:36 -0500

    Se agrega la vista de la terapeuta

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use((req, res, next) => {
 });
 
 app.post('/validateUser', function(req, resp){
-    debugger;
+    //debugger;
     db.collection("Usrs").find(req.body).toArray((error, docs) => {
 
         var sResponseUsr = null;
@@ -59,7 +59,7 @@ app.post('/validateUser', function(req, resp){
             };
 
         } else {
-            debugger;
+            //debugger;
             if (docs.length == 1) {
 
                 var sViewName = "";
@@ -75,7 +75,7 @@ app.post('/validateUser', function(req, resp){
 
                 sResponseUsr = {
                     success : {
-                        sesionId : docs[0].id + "|" +docs[0].id_massagePoint + "|8912hdn|p28647|901218b",
+                        sesionId : docs[0].id + "|" + docs[0].id_massagePoint + "|8912hdn|p28647|901218b",
                         view : sViewName
                     }
                 };
@@ -89,5 +89,212 @@ app.post('/validateUser', function(req, resp){
         }     
 
         resp.send(sResponseUsr);   
+    });
+});
+
+app.post('/therapists', function(req, resp){
+    //debugger;
+    var sResponseUsr = null;
+
+    if (req.body.session !== undefined && req.body.session !== null &&
+            req.body.session !== "") {
+
+        var aData = req.body.session.split('|');
+
+        db.collection("Usrs").find({id_massagePoint : aData[1]}).toArray((error, docs) => {
+
+            if (error) {
+                console.log(error);
+                sResponseUsr = {
+                    error : error.message
+                };
+
+            } else {
+                //debugger;
+                if (docs.length > 0) {
+
+                    var users  = [];
+
+                    docs.forEach(function(user) {
+
+                        var usrTmp = {
+                            id : user.id,
+                            name : user.name
+                        };
+
+                        users.push(usrTmp);
+                    });
+
+                    sResponseUsr = {
+                        success : {
+                            therapists : users
+                        }
+                    };
+
+                } else {
+
+                    sResponseUsr = {
+                        error : "No hay terapeutas para el centro de masaje seleccionado."
+                    };
+                }
+            }     
+
+            resp.send(sResponseUsr);   
+        });
+    } else {
+        
+        sResponseUsr = {
+            error : "Peticion incompleta, intente nuevamente."
+        };
+        
+        resp.send(sResponseUsr);  
+    }
+});
+
+app.post('/massagePoint', function(req, resp){
+    //debugger;
+    var sResponse = null;
+
+    if (req.body.session !== undefined && req.body.session !== null &&
+            req.body.session !== "") {
+
+        var aData = req.body.session.split('|');
+
+        db.collection("MassagePoint").find({id : aData[1]}).toArray((error, docs) => {
+
+            if (error) {
+                console.log(error);
+                sResponse = {
+                    error : error.message
+                };
+
+            } else {
+                //debugger;
+                if (docs.length == 1) {
+
+                    sResponse = {
+                        success : {
+                            pointName : docs[0].name,
+                            pointLocation : docs[0].location
+                        }
+                    };
+
+                } else {
+
+                    sResponse = {
+                        error : "Existen dos o mas sucursales."
+                    };
+                }
+            }     
+
+            resp.send(sResponse);   
+        });
+
+    } else {
+        
+        sResponse = {
+            error : "Peticion incompleta, intente nuevamente."
+        };
+        
+        resp.send(sResponse);  
+    }
+});
+
+app.get('/creams', function(req, resp){
+    //resp.send('this is a GET');
+    var creamTypes = [];
+    var creams = [];
+    var sResponse = null;
+
+    db.collection("CreamType").find({status : "1"}).toArray((error, docs) => {
+
+        if (error) {
+            console.log(error);
+            sResponse = {
+                error : error.message
+            };
+
+        } else {
+            //debugger;
+            if (docs.length > 0) {
+
+                creamTypes = docs;
+
+                db.collection("Cream").find({status : "1"}).toArray((errorCream, docsCream) => {
+
+                    if (errorCream) {
+                        console.log(errorCream);
+                        sResponse = {
+                            error : errorCream.message
+                        };
+            
+                    } else {
+                        //debugger;
+                        if (docsCream.length > 0) {
+            
+                            creams = docsCream;
+                            
+                            var creamsResp = [];
+
+                            creams.forEach(function(cream) {
+                                //debugger;
+                                /*var creamTmp = {
+                                    id : '',
+                                    name : '',
+                                    tipo : '',
+                                    subType : '',
+                                    week : false
+                                }*/
+                                var creamTmp = {
+                                    name : '',
+                                    tipo : '',
+                                    week : false
+                                }
+                                
+                                creamTmp.name = cream.name;
+
+                                var foundType = false;
+                                var iRecTypes = 0;
+                                while (!foundType && iRecTypes < creamTypes.length) {
+
+                                    if (creamTypes[iRecTypes].id === cream.id_creamType) {
+
+                                        foundType = true;
+                                        creamTmp.tipo = creamTypes[iRecTypes].name;
+
+                                    } else {
+                                        
+                                        iRecTypes = iRecTypes + 1;
+                                    }
+                                }
+
+                                creamsResp.push(creamTmp);
+
+                            });
+                            sResponse = {
+                                success : {
+                                    creams : creamsResp
+                                }
+                            };
+            
+                        } else {
+            
+                            sResponse = {
+                                error : "No existen cremas activas."
+                            };
+                        }
+                    }          
+
+                    resp.send(sResponse);   
+            
+                });
+
+            } else {
+
+                sResponse = {
+                    error : "No existen tipos de crema activos."
+                };
+            }
+        }
     });
 });
